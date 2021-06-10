455ed781640e2271440718895255eb18977566f1
Süleyman Özyılmaz <48279213+suleymanozyilmaz@users.noreply.github.com>
2021-06-10 21:56:19 +0300

    deneme
    deneme commit

--- YunusEmre_Caliskan_Homework5/src/yunusemre_caliskan_homework5/YunusEmre_Caliskan_Homework5.java
+++ YunusEmre_Caliskan_Homework5/src/yunusemre_caliskan_homework5/YunusEmre_Caliskan_Homework5.java
@@ -17,6 +17,7 @@ public class YunusEmre_Caliskan_Homework5 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("ASD");
+        System.out.println("Suleyman Ozyilmaz")
     }
     
 }
