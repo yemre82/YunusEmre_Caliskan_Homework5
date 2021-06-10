b2d2cae7c67efc06fa94cd2bca7175ec8267e95a
yemre82 <51782698+yemre82@users.noreply.github.com>
2021-06-10 21:53:58 +0300

    Yunus Emre Commit New

--- YunusEmre_Caliskan_Homework5/src/yunusemre_caliskan_homework5/YunusEmre_Caliskan_Homework5.java
+++ YunusEmre_Caliskan_Homework5/src/yunusemre_caliskan_homework5/YunusEmre_Caliskan_Homework5.java
@@ -16,6 +16,7 @@ public class YunusEmre_Caliskan_Homework5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("ASD");
     }
     
 }
